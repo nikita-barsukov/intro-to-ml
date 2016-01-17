8116fb17a20e501b4e2111c7c1be5e5a69a13bf8
Nikita Barsukov <nikita@barsukov.net>
2016-01-17 22:02:01 +0100

    Feature scaling

--- poi_id.py
+++ poi_id.py
@@ -12,7 +12,7 @@ import pandas as pd
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','total_payments', 'from_poi_to_this_person']
+features_list = ['poi','total_payments', 'from_poi_to_this_person', 'to_messages']
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -29,9 +29,22 @@ del(data_dict['TOTAL'])
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
+def scale_feature(x, xmin, xmax):
+    if x == 'NaN':
+        return 0
+    else:
+        return (float(x) - xmin) / (xmax - xmin)
+
+for feature in features_list:
+    xmin = min(float(my_dataset[datapoint][feature]) for datapoint in my_dataset)
+    xmax = max(float(my_dataset[datapoint][feature]) for datapoint in my_dataset)
+    for datapoint in my_dataset:
+        my_dataset[datapoint][feature] = scale_feature(my_dataset[datapoint][feature], xmin, xmax)
+
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
+
 labels, features = targetFeatureSplit(data)
 
 ### Task 4: Try a varity of classifiers
